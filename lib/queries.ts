e0f62325f933befae4171bc94d5608ddd2705a98
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2024-12-02 23:50:23 +0100

    finished details page and used PPR to rended the number of views

--- lib/queries.ts
+++ lib/queries.ts
@@ -5,3 +5,5 @@ export const STARTUPS_QUERY = defineQuery(
 ); 
 
 export const STARTUP_BY_ID_QUERY = defineQuery('*[_type == "startup" && _id == $id][0]{_id, title, slug, _createdAt, author -> {_id, username, image, bio}, views, description, category, image, pitch}'); 
+
+export const STARTUP_VIEWS_QUERY = defineQuery('*[_type == "startup" && _id == $id][0]{_id, views}')
