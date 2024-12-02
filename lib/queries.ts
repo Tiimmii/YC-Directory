b1f433e0a5a07e88ce778a71b29de9f11ebb3d5a
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2024-12-02 22:57:33 +0100

    fixed 100 error :(

--- lib/queries.ts
+++ lib/queries.ts
@@ -4,4 +4,4 @@ export const STARTUPS_QUERY = defineQuery(
     '*[_type == "startup" && defined(slug.current) && ! defined($search) || title match $search || category match $search || author-name match $search] | order(_createdAt desc){_id, title, slug, _createdAt, author -> {_id, username, image, bio}, views, description, category, image}'
 ); 
 
-export const STARTUP_BY_ID_QUERY = defineQuery('*[_type == "startup" && _id = $id][0]{_id, title, slug, _createdAt, author -> {_id, username, image, bio}, views, description, category, image, pitch}');
+export const STARTUP_BY_ID_QUERY = defineQuery('*[_type == "startup" && _id == $id][0]{_id, title, slug, _createdAt, author -> {_id, username, image, bio}, views, description, category, image, pitch}'); 
